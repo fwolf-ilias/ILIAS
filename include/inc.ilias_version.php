f22a549db2fba89a97f0a9f2566228277c0d78fc
www-data <www-data@release.ilias.de>
2024-02-15 18:22:28 +0000

    Release 8.9a

--- include/inc.ilias_version.php
+++ include/inc.ilias_version.php
@@ -10,5 +10,5 @@
 *
 * @package ilias-core
 */
-define("ILIAS_VERSION", "8.9 2024-02-14");
-define("ILIAS_VERSION_NUMERIC", "8.9");			// since version ILIAS 6 this must be always x.y: x and y are numbers
+define("ILIAS_VERSION", "8.9a 2024-02-15");
+define("ILIAS_VERSION_NUMERIC", "8.9a");			// since version ILIAS 6 this must be always x.y: x and y are numbers
--- setup/sql/ilias3.sql
+++ setup/sql/ilias3.sql
@@ -13120,7 +13120,7 @@ INSERT INTO `object_data` VALUES (34,'typ','lm','Learning module Object',-1,'200
 INSERT INTO `object_data` VALUES (35,'typ','notf','Note Folder Object',-1,'2002-12-21 00:04:00','2002-12-21 00:04:00','',NULL);
 INSERT INTO `object_data` VALUES (36,'typ','note','Note Object',-1,'2002-12-21 00:04:00','2002-12-21 00:04:00','',NULL);
 INSERT INTO `object_data` VALUES (37,'typ','frm','Forum object',-1,'2002-07-15 15:54:22','2003-08-15 12:36:40','',NULL);
-INSERT INTO `object_data` VALUES (70,'lng','en','installed',-1,NULL,'2024-02-14 15:58:32','',NULL);
+INSERT INTO `object_data` VALUES (70,'lng','en','installed',-1,NULL,'2024-02-15 18:20:51','',NULL);
 INSERT INTO `object_data` VALUES (71,'lng','de','not_installed',6,'2003-08-15 10:25:19','2015-12-22 16:29:24','',NULL);
 INSERT INTO `object_data` VALUES (72,'lng','es','not_installed',6,'2003-08-15 10:25:19','2003-08-15 10:25:19','',NULL);
 INSERT INTO `object_data` VALUES (73,'lng','it','not_installed',6,'2003-08-15 10:25:19','2003-08-15 10:25:19','',NULL);
@@ -25097,4 +25097,4 @@ CREATE TABLE `xmlvalue_seq` (
 
 
 
--- Dump completed on 2024-02-14 15:58:33
+-- Dump completed on 2024-02-15 18:20:52
